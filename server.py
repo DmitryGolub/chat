95b7379182cac6f9771278af49f13e009c825d57
Dmitry Golub <gosest4@gmail.com>
2025-08-24 00:45:06 +0300

    init project

---
+++ server.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+app = FastAPI()
+
+
+clients = []
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Широковещательная рассылка
+            for client in clients:
+                await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+
+
+@app.get("/")
+async def get():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
